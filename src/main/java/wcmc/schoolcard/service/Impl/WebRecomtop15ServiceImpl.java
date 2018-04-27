14623ee2147ff22a5b9f071472bf3cf73d0ee7dc
Channings <chen_lingchi@163.com>
2018-04-27 21:25:49 +0800

    20180427完善个性化标签和推荐

--- src/main/java/wcmc/schoolcard/service/Impl/WebRecomtop15ServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package wcmc.schoolcard.service.Impl;
-
-/**
- * Created by Channings on 18/4/27.
- */
-public class WebRecomTop15ServiceImpl {
-}
--- src/main/java/wcmc/schoolcard/service/WebRecomtop15Service.java
+++
@@ -1,7 +0,0 @@
-package wcmc.schoolcard.service;
-
-/**
- * Created by Channings on 18/4/27.
- */
-public class WebRecomTop15Service {
-}
